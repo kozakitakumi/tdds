6ebfeced31db4f25943780f90c4b0135736fd902
kozakitakumi <k.takumi506@gmail.com>
2022-10-30 19:29:31 +0900

    第1部の3章まで

--- src/money/MoneyTest.java
+++ src/money/MoneyTest.java
@@ -12,4 +12,11 @@ public class MoneyTest {
         five.times(3);
         assertEquals(15, product.amount);
     }
+
+    @Test
+    public void testEquality(){
+        assertTrue(new Dollar(5).equals(new Dollar(5)));
+        assertFalse(new Dollar(5).equals(new Dollar(6)));
+    }
+
 }
